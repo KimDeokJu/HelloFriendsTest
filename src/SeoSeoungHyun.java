ec698e647a6ecbbc4c1df89e34a111061bb4b659
KimDeokJu <darkzeus86@gmail.com>
2016-06-09 18:27:36 +0900

    doWork created implemented

--- src/SeoSeoungHyun.java
+++ src/SeoSeoungHyun.java
@@ -1,5 +1,10 @@
-/**
- * 승현이형 할일
- */
 public class SeoSeoungHyun {
+
+    public static void main(String[] args) {
+        doWork();
+    }
+
+    public static void doWork() {
+        System.out.println("나는 모든걸 다 하지. 메디브승현");
+    }
 }
